2b8669fe030dde19958a1d35c9e1b41cea007628
CreaTorAlexander <Hess.alexander1@web.de>
2024-01-29 11:49:29 +0100

    Add basic structure of the portfolio app

---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  standalone: true,
+  templateUrl: './navigation-bar.component.html',
+  styleUrl: './navigation-bar.component.css'
+})
+export class NavigationBarComponent {
+
+}
